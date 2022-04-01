5ab019acbd05eb074bc761d95648a95a7af2ee6d
Felipe Almeida <felipegoa@hotmail.com>
2022-03-31 21:30:01 -0300

    feat: first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,16 +51,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
         </Head>
         <body>
           <Main />
